82fc8e5fb7b16ee4c698a227bed7bf7eee41e973
anandoh-gst <z.nandoh@gst-srl.com>
2024-08-28 10:50:19 +0200

    Aggiunta la cartella Portfolio

---
+++ Portfolio_Project/FE/personal-portfolio/src/App.js
@@ -0,0 +1,108 @@
+
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
+import Testimonials from "./scenes/Testimonials";
+import Projects from "./scenes/Projects";
+import MySkills from "./scenes/MySkills";
+import LineGradient from "./components/LineGradient";
+import { motion } from "framer-motion"; 
+import Landing from "./scenes/Landing";
+import Navbar from "./scenes/NavBar";
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import DotGroup from "./scenes/DotGroup";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        if (selectedPage !== "home") {
+          setSelectedPage("home");
+        }
+        // console.log("🚀 ~ handleScroll ~ selectedPage:", selectedPage)
+        
+      } else {
+        setIsTopOfPage(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [ selectedPage ]);
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+
+      <div className="w-5/6 mx-auto md:h-full">
+        {isDesktop && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <motion.div
+          // margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("home")}
+          // onViewportLeave={() => selectedPage === "home" && setSelectedPage("")}
+        >
+          <Landing setSelectedPage={setSelectedPage} />
+        </motion.div>
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full ">
+        <motion.div
+          // margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("skills")}
+        >
+          <MySkills />
+        </motion.div>
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <motion.div
+          // margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("projects")}
+        >
+          <Projects />
+        </motion.div>
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <motion.div
+          // margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("testimonials")}
+        >
+          <Testimonials />
+        </motion.div>
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <motion.div
+          // margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("contact")}
+        >
+          <Contact />
+        </motion.div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
